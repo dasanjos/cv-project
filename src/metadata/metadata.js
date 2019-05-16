c3213e2c6b5c73dd9ba9bfe52dd837f68979b97d
Daniel Anjos <daniel.anjos@roguewave.com>
2019-05-16 06:13:13 +0300

    Updating format and content

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -3,7 +3,7 @@ module.exports = {
   title: 'Software Engineering Manager / Team Lead',
   baseUrl: 'https://dasanjos.github.io/cv',
   facts: {
-    'Address': 'Tallinn, Estonia',
+    'Address': 'Vibu 6 - 66, 10415 Tallinn, Estonia',
     'Telephone': '+372 5553 5270',
     'Email': '<a href="mailto:dasanjos@gmail.com">dasanjos@gmail.com</a>'
   },
@@ -27,54 +27,42 @@ module.exports = {
       title: 'Engineering Manager',
       written: 'Aug 2018 – present',
       badges: ['Agile', 'Java', 'CI/CD', 'AWS', 'SaaS', 'DevOps', 'Docker'],
-      contents: `
-Leading a multi product team of Software engineers with Agile methodologies. Oversee and improve software development methods, conventions and practices. Software development and migration to modern architectures.
-`
+      contents: `Created the engineering site and lead a multi product team of Software engineers and Software engineers in Testing. Improved software development practices by introducing Docker and CI/CD pipelines with Jenkins.`
     },
     {
-      company: 'ZeroTurnaround',
+      company: 'JRebel - a Perforce company',
       title: 'Software Engineering Team Lead',
       written: 'Aug 2014 – Jul 2018',
       badges: ['Agile', 'Java', 'CI/CD', 'TDD', 'Pair Programming'],
-      contents: `
-Leading a multi product team of Software engineers with Agile methodologies. Oversee and improve software development methods, conventions and practices. Application architecture, development and migration to modern architectures.
-`
+      contents: `Leading a multi product team of Software engineers with Agile methodologies. Oversee and improve software development methods, conventions and practices. Application architecture, development and migration to modern architectures.`
     },
     {
       company: 'CGI Germany',
       title: 'Lead CQ5/AEM Developer Consultant',
       written: 'Apr 2014 – Jul 2014',
       badges: ['Java', 'AEM6', 'HTML5', 'JavaScript', 'CSS3'],
-      contents: `
-Design, estimate and implement business solutions using CQ5/AEM6 as a platform. Implement Vodafone corporate site with AEM6.
-`
+      contents: `Design, estimate and implement business solutions using CQ5/AEM6 as a platform. Implement Vodafone corporate site with AEM6.`
     },
     {
       company: 'Swedbank Estonia',
       title: 'Software Developer and Scrum Master',
       written: 'Nov 2012 – Apr 2014',
       badges: ['Agile', 'JavaEE', 'Spring', 'Hibernate'],
-      contents: `
-Software development on internet banking platform with Backend development in JavaEE, Spring and Hibernate and Frontend development in HTML5, CSS3 and JavaScript for desktop and mobile devices.
-`
+      contents: `Software development (backend and frontend) with Internet Banking team. Implemented Latvian and Lithuanian digital signature (BDOC) for authentication and payments online.`
     },
     {
       company: 'Skype',
       title: 'Software Development Engineer',
       written: 'Jun 2010 – Nov 2012',
       badges: ['Agile', 'Java', 'CQ5', 'HTML5', 'JavaScript', 'CSS3' ],
-      contents: `
-Joined the Skype Frontend (Skype.com) team to help transition Skype from static site to a Java based content management system (CQ5). 
-`
+      contents: `Software development (Backend and Frontend) with Skype Frontend team. Transitioned Skype.com from static site to a Java based content management system (CQ5) resulting in 10x faster content turnaround.`
     },
     {
       company: 'Aqris Software AS',
       title: 'Java Developer and Scrum Master',
       written: 'Oct 2008 – Jun 2010',
       badges: ['Agile', 'Spring', 'Hibernate'],
-      contents: `
-Developer, Scrum Master and Tester for internal and customer projects with multiple web technologies. 
-`
+      contents: `Developer, Scrum Master and Tester for internal and customer projects with multiple web technologies. Lead and delivered Skype internal documentation repository migration project from start to end.`
     }
   ],
   certifications: [
